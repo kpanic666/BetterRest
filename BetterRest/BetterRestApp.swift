0780d690fa5af062e5310b9bf0e18cb8a9fef81c
Andrei Korikov <kpanic666@gmail.com>
2021-07-12 22:42:30 +0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andrei Korikov on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
